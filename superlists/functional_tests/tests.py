4f1a360c040ec6f48c2bd8a292c24d2d79e34f56
Abby Meunier <abby.meunier@gmail.com>
2016-04-08 14:16:13 -0700

    First steps of FT for layout and styling

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -77,10 +77,29 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         #Satisfied, they both go back to sleep
-
         #Abby sees that the site has generated a unique URL for her - there is text explaining this
-
         #She visits that URL, and her to-do list is still there
-
         #She leaves the site
+
+    def test_layout_and_styling(self):
+        #Abby goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        #She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual( 
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5 #plus or minus 5 pixels
+        )
+
+        #She starts a new list and sees the input is nicely centered there, too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
 
